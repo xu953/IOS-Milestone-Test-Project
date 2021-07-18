e2a190d324d5f0d8e05c3dea03fb38d0e0a64693
Sophia Chen <sophia@Sophias-MacBook-Air.local>
2021-07-18 19:13:19 -0400

    added Storyboard lesson 1-3 tasks

---
+++ lesson2/lesson2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson2
+//
+//  Created by Sophia Chen on 7/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
