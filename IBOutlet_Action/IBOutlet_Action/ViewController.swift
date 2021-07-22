5326f6f1210a0e2f8c9d618fa027d3943c49da0f
Sophia Chen <sophia@Sophias-MacBook-Air.local>
2021-07-22 18:55:24 -0400

    added IBOutlet and IBAction task

---
+++ IBOutlet_Action/IBOutlet_Action/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IBOutlet_Action
+//
+//  Created by Sophia Chen on 7/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
